675a7b080dd329466a40b095f3aef91621c3a47b
sly-designs <sylvestorsielei@gmail.com>
2022-10-31 22:25:42 +0300

    department list

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,11 +1,28 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-department-list',
-  templateUrl: './department-list.component.html',
-  styleUrls: ['./department-list.component.css']
+  template: `<h3>
+    Department List
+  </h3>
+  <ul class="items">
+     <li *ngFor="let department of department">
+         <span class="badge">{{department.id}}</span> {{department.name}}
+     </li>
+  </ul>
+  `,
+  styles: []
 })
 export class DepartmentListComponent implements OnInit {
+
+  department = [
+    {"id": 1, "name": "Information Security"},
+    {"id": 2, "name": "Navision"},
+    {"id": 3, "name": "Software Development"},
+    {"id": 4, "name": "Management"},
+    {"id": 5, "name": "Business"},
+  ]
 
   constructor() { }
 
